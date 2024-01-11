6f78a1824cceee009bc55f1f18e5fae0050eeae1
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2024-01-11 16:33:57 +0500

    5.1-dars

---
+++ app.js
@@ -0,0 +1,83 @@
+// try {
+//     alert('salom')
+//     console.log('ozodbek');
+//     console.log(a);
+//     alert('jhdf')
+// } catch (error) {
+//     console.log('xatolik yuzaga keldi',error);
+// }
+
+// setTimeout(( ) => {
+//     console.log('ozodbek');
+// }, 3000)
+
+// setInterval(() => {
+//     console.log('kebdozo');
+// }, 1500)
+
+// try {
+//     console.log('ozodbek');
+//     console.log(ozodbeek);
+// } catch (error) {
+//     // console.log(error.name);
+//     // console.log(error.message);
+//     console.log(error.stakc);
+// }
+
+// import {sum} from "./main.js";
+// console.log(sum(3, 7));
+
+
+
+// 1 - masala
+
+// let str = [1, 2, "salom", "hayr", true, 4, 5, 6, 7];
+
+// function arr(arg) {
+//    let max = 0;
+//    for (let i = 0; i < arg.length; i++) {
+//      if (typeof arg[i] === 'number') {
+//        max += arg[i];
+//      }
+//    }
+//    return max;
+// }
+//  console.log(arr(str));
+
+// 2 - masala
+
+// let str = ["dengiz", "ogabek", "ozodbek"];
+
+// function arr(arg) {
+// return arg.map(value => {
+//       if (value.length > 0) {
+//         return value[0].toUpperCase() + value.slice(1);
+//       } else {
+//         return value;
+//       }
+//     });
+// }  
+// console.log(arr(str)); 
+
+// 3 - masala
+
+// let a = 224422
+
+// function arr(arg) {
+//     let reserved = 0;
+//     let max = arg;
+//     while (max !== 0) {
+//         reserved = reserved*10 + max % 10;
+//         max = Math.floor(max/10)
+//     }
+//     let s = 'bu son palindrom emas'
+//     if (arg === reserved) {
+//         s = 'son palindrom'
+//     }
+//     return s
+// }
+// console.log(arr(a));
+
+  
+
+  
